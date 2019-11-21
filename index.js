9dc6837b6ea4b5927fdbb68f2fde1c27b0da16f1
guotao <2082226872@qq.com>
2019-11-21 16:43:11 +0800

    add-touch

--- index.js
+++ index.js
@@ -63,43 +63,87 @@ function listenToMouse(canvas) {
         x: undefined,
         y: undefined
     }
-    canvas.onmousedown = function(aaa) {
-        var x = aaa.clientX
-        var y = aaa.clientY
-        using = true
-        if (eraserEnabled) {
-            context.clearRect(x - 5, y - 5, 10, 10) //如果点击橡皮擦则清除x,y
-        } else {
-            lastPoint = {
-                "x": x,
-                "y": y
-            }
-        }
 
-
-        console.log(lastPoint)
-            // drawcircle(x, y, 1)
-    }
-    canvas.onmousemove = function(aaa) {
-        var x = aaa.clientX
-        var y = aaa.clientY
-
-        if (!using) { //如果没有点击橡皮擦则直接return
-            return
-        }
-        if (eraserEnabled) {
-            context.clearRect(x - 5, y - 5, 10, 10)
-        } else {
-            var newPoint = {
+    //特性检测
+    if (document.body.ontouchstart !== undefined) { //监听是否可以使用ontouchstart这个方法,如果不为undefined则为触屏设备
+        //触屏设备
+        canvas.ontouchstart = function(aaa) {
+            console.log(aaa); //查看传递的内容找到x,y坐标
+            var x = aaa.touches[0].clientX
+            var y = aaa.touches[0].clientY
+            console.log(x, y);
+            using = true
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10) //如果点击橡皮擦则清除x,y
+            } else {
+                lastPoint = {
                     "x": x,
                     "y": y
                 }
-                // drawcircle(x, y, 1)
-            drawline(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-            lastPoint = newPoint //实时更新最后一个点
+            }
         }
-    }
-    canvas.onmouseup = function(aaa) {
-        using = false
+        canvas.ontouchmove = function(aaa) {
+            var x = aaa.touches[0].clientX
+            var y = aaa.touches[0].clientY
+
+            if (!using) { //如果没有点击橡皮擦则直接return
+                return
+            }
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                var newPoint = {
+                        "x": x,
+                        "y": y
+                    }
+                    // drawcircle(x, y, 1)
+                drawline(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint //实时更新最后一个点
+            }
+        }
+        canvas.ontouchend = function() {
+            using = false
+        }
+    } else {
+        //非触屏设备
+        canvas.onmousedown = function(aaa) {
+            var x = aaa.clientX
+            var y = aaa.clientY
+            using = true
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10) //如果点击橡皮擦则清除x,y
+            } else {
+                lastPoint = {
+                    "x": x,
+                    "y": y
+                }
+            }
+
+
+            console.log(lastPoint)
+                // drawcircle(x, y, 1)
+        }
+        canvas.onmousemove = function(aaa) {
+            var x = aaa.clientX
+            var y = aaa.clientY
+
+            if (!using) { //如果没有点击橡皮擦则直接return
+                return
+            }
+            if (eraserEnabled) {
+                context.clearRect(x - 5, y - 5, 10, 10)
+            } else {
+                var newPoint = {
+                        "x": x,
+                        "y": y
+                    }
+                    // drawcircle(x, y, 1)
+                drawline(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+                lastPoint = newPoint //实时更新最后一个点
+            }
+        }
+        canvas.onmouseup = function(aaa) {
+            using = false
+        }
     }
 }
